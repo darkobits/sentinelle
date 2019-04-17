40eb4f7c3d00fa003e893d2abb6adb0836672a89
darkobits <joshua@frontlawn.net>
2019-04-17 04:10:53 -0700

    chore: Update build script.

--- etc/build-image.js
+++ etc/build-image.js
@@ -7,60 +7,56 @@ const env = require('@darkobits/env').default;
 /**
  * Run the provided command, routing output to standard out.
  */
-function run(cmd) {
-  return execSync(cmd, {
-    stdio: 'inherit',
+const run = cmd => execSync(cmd, {stdio: 'inherit', cwd: process.cwd()});
+
+
+/**
+ * If a single Git tag points at the current HEAD, returns it. If zero tags or
+ * multiple tags point at the current HEAD, returns `false`.
+ */
+function tagAtHead() {
+  const results = execSync('git tag --points-at=HEAD', {
+    encoding: 'utf8',
     cwd: process.cwd()
-  });
+  }).trim().split('\n');
+
+  if (!results || results.length !== 1) {
+    return false;
+  }
+
+  return results[1];
 }
 
 
 /**
- * If a single Git tag points at the current HEAD, outputs it. If zero tags or
- * multiple tags point at the current HEAD, exits with code 1.
+ * If there is a Git tag pointing to the current HEAD, builds Docker images and
+ * pushes them to Docker Hub.
  */
-function tagAtHead() {
-  const resultLines = execSync('git tag --points-at=HEAD', {
-    encoding: 'utf8',
-    cwd: process.cwd()
-  }).trim();
-
-  if (!resultLines) {
-    return false;
-  }
-
-  const results = resultLines.split('\n');
-
-  if (results.length !== 1) {
-    return false;
-  }
-
-  return results[0];
-}
-
-
 function buildAndPushImage() {
-  run(`echo ${env('DOCKER_PASSWORD')} | docker login --username ${env('DOCKER_USERNAME')} --password-stdin`);
-
-  const tagsToPush = [];
-
-  const baseImageName = 'darkobits/sentinelle';
-
-  run(`docker build . --tag=${baseImageName}`);
-
-  tagsToPush.push(baseImageName);
-
   const gitTag = tagAtHead();
 
-  if (gitTag) {
-    const gitTagImageName = `darkobits/sentinelle:${gitTag}`;
-    run(`docker tag darkobits/sentinelle:latest ${gitTagImageName}`);
-    console.log(`Successfully tagged ${gitTagImageName}`);
-    tagsToPush.push(gitTagImageName);
+  if (!gitTag) {
+    console.log('[build-image] No tags at HEAD; skipping.');
+    return;
   }
 
+  // Log-in to Docker Hub.
+  run(`echo ${env('DOCKER_PASSWORD', true)} | docker login --username ${env('DOCKER_USERNAME', true)} --password-stdin`);
+
+  // Compute base image and versioned image names.
+  const baseImageName = 'darkobits/sentinelle';
+  const gitTagImageName = `${baseImageName}:${gitTag}`;
+  const tags = [baseImageName, gitTagImageName];
+
+  // Build base image.
+  run(`docker build . --tag=${baseImageName}`);
+
+  // Tag and push images.
   tagsToPush.forEach(tag => {
+    run(`docker tag ${baseImageName} ${tag}`);
+    console.log(`[build-image] Successfully tagged ${tag}`);
     run(`docker push ${tag}`);
+    console.log(`[build-image] Successfully pushed ${tag}`);
   });
 }
 
