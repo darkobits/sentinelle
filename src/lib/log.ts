db10270a1808c0b4450dfc9746f8d4ce0983488a
darkobits <joshua@frontlawn.net>
2019-07-01 16:42:28 -0700

    chore: Misc. cleanup.

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -8,12 +8,5 @@ const log = LogFactory('', level);
 log.heading = '\u001b[38;5;44ms\u001b[39m\u001b[38;5;44me\u001b[39m\u001b[38;5;44mn\u001b[39m\u001b[38;5;44mt\u001b[39m\u001b[38;5;44mi\u001b[39m\u001b[38;5;43mn\u001b[39m\u001b[38;5;43me\u001b[39m\u001b[38;5;43ml\u001b[39m\u001b[38;5;43ml\u001b[39m\u001b[38;5;43me\u001b[39m';
 Reflect.deleteProperty(log.headingStyle, 'bg');
 
-// Enable color in Docker.
-if (env('IS_DOCKER')) {
-  log.enableColor();
-  log.chalk.enabled = true;
-  log.chalk.level = 2;
-}
-
 
 export default log;
