c25a92bd1748bd469db60d8b5f9e4b50f7b81fb4
Joshua <joshua@collectivehealth.com>
2019-10-04 14:37:10 -0700

    chore: Update log style.

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -8,6 +8,11 @@ log.configure({
   heading: '\u001b[38;5;44ms\u001b[39m\u001b[38;5;44me\u001b[39m\u001b[38;5;44mn\u001b[39m\u001b[38;5;44mt\u001b[39m\u001b[38;5;44mi\u001b[39m\u001b[38;5;43mn\u001b[39m\u001b[38;5;43me\u001b[39m\u001b[38;5;43ml\u001b[39m\u001b[38;5;43ml\u001b[39m\u001b[38;5;43me\u001b[39m',
   style: {
     heading: token => token
+  },
+  levels: {
+    info: {
+      style: (token, chalk) => chalk.keyword('mediumseagreen')(token)
+    }
   }
 });
 
